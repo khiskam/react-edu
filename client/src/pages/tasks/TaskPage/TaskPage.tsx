4e5c5564dac5588877227178dd12a625f53237e3
Kamilla Khisamieva <khisamieva0409@gmail.com>
2024-03-18 23:17:38 +0300

    replace checkbpx to tag

--- client/src/pages/tasks/TaskPage/TaskPage.tsx
+++ client/src/pages/tasks/TaskPage/TaskPage.tsx
@@ -1,6 +1,6 @@
-import { Button, Checkbox, Col, Flex, Modal, Row, Typography } from "antd";
+import { Button, Col, Flex, Modal, Row, Tag, Typography } from "antd";
 import { useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { ROUTES } from "src/shared/constants";
 import { SPACE } from "src/shared/theme";
 import { Container, PageLayout } from "src/shared/ui";
@@ -19,24 +19,29 @@ export const TaskPage = () => {
   const item = data[taskId ? +taskId : 1];
   return (
     <Container>
-      <PageLayout>
+      <PageLayout gap={32}>
         <Row gutter={[SPACE.gap12, SPACE.gap12]}>
-          <Col>
+          <Col span={24}>
             <Typography.Title level={2}>{item.title}</Typography.Title>
           </Col>
+
+          <Col span={24}>
+            <Tag color={item.isCompleted ? "blue" : "red"}>
+              {item.isCompleted ? "выполнено" : "необходимо выполнить"}
+            </Tag>
+          </Col>
+
           <Col span={24}>
             <Typography.Text>{item.descr}</Typography.Text>
           </Col>
-          {item.isCompleted ? (
-            <Col span={24}>
-              <Checkbox disabled>Выполнено</Checkbox>
-            </Col>
-          ) : null}
+
           <Col span={24}>
             <Typography.Text strong>Дата изменения: </Typography.Text>
-            <Typography.Text>01.01.2024</Typography.Text>
+            <Typography.Text>
+              {item.createdAt.toLocaleDateString()}
+            </Typography.Text>
           </Col>
-          <Col>
+          <Col span={24}>
             <Flex gap={SPACE.gap4}>
               <Button type="primary" onClick={goToEditPage}>
                 Редактировать
@@ -47,6 +52,7 @@ export const TaskPage = () => {
             </Flex>
           </Col>
         </Row>
+        <Link to={ROUTES.tasks}>← Ко всем задачам</Link>
       </PageLayout>
 
       <Modal
